e1a7acdd5903de6c846b9a58645219fc5e6fd865
sk9212k <sk9212k@gmail.com>
2023-10-27 14:19:17 +0000

    added main.tf and variables.tf to terraform folder

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "hobbes"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "westeurope"
+}
+
+
