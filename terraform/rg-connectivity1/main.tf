bd1f2e1bfaebb2f2ddf2be4b1633fc67c008a26a
sk9212k <sk9212k@gmail.com>
2023-11-06 16:16:15 +0000

    I've added the vpn configuration to connect from my on premises environment to azure cloud

--- terraform/rg-connectivity1/main.tf
+++ terraform/rg-connectivity1/main.tf
@@ -24,3 +24,47 @@ resource "azurerm_subnet" "subnets" {
   ] 
 }
 
+
+resource "azurerm_public_ip" "VPN-PublicIP" {
+  name                = "pip-vgw-connectivity-001"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  allocation_method = "Dynamic"
+  depends_on = [azurerm_virtual_network.vnet]
+}
+
+resource "azurerm_virtual_network_gateway" "VPN-Gateway" {
+  name                = "vgw-connectivity-001"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vgw-config"
+    public_ip_address_id          = azurerm_public_ip.VPN-PublicIP.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.subnets["GatewaySubnet"].id
+  }
+  timeouts {
+    create = "120m"
+  }
+}
+
+resource "azurerm_local_network_gateway" "LocalGateway" {
+  name                = "lgw-onpremises-001"
+  location            = azurerm_virtual_network.vnet.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  gateway_address     = var.LocalGateway.gateway_address
+  address_space       = [var.LocalGateway.subnet2,var.LocalGateway.subnet1]
+}
+
+
+
+
--- terraform/rg-connectivity1/variables.tf
+++ terraform/rg-connectivity1/variables.tf
@@ -31,3 +31,35 @@ variable "Subnets" {
         }
     }
 }
+
+variable "LocalGateway" {
+  type = map
+  default = {
+    "gateway_address"                 = "92.27.153.75"
+    "subnet1"                         = "192.168.1.0/24"
+    "subnet2"                         = "192.168.10.0/24"    
+  }  
+}
+
+data "azurerm_key_vault" "kv-cloudninja-vpn-001" {
+  name                = "kv-cloudninja-vpn-001"
+  resource_group_name = "rg-keyvault-001"
+}
+
+data "azurerm_key_vault_secret" "VPNSharedSecret" {
+  name         = "VPNSharedSecret"
+  key_vault_id = data.azurerm_key_vault.kv-cloudninja-vpn-001.id
+}
+
+resource "azurerm_virtual_network_gateway_connection" "VPN-Connection" {
+  name                = "vcn-onpremises-001"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.VPN-Gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.LocalGateway.id
+
+  shared_key = data.azurerm_key_vault_secret.VPNSharedSecret.value
+}
+
