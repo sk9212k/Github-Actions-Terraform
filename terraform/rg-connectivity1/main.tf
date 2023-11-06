a040cebdcdffb34144712c361feef50a0a1c11b8
sk9212k <sk9212k@gmail.com>
2023-11-06 17:58:45 +0000

    ZB-8 I've added more changes to configure the vpn connection

--- terraform/rg-connectivity1/main.tf
+++ terraform/rg-connectivity1/main.tf
@@ -65,6 +65,25 @@ resource "azurerm_local_network_gateway" "LocalGateway" {
   address_space       = [var.LocalGateway.subnet2,var.LocalGateway.subnet1]
 }
 
+data "azurerm_key_vault" "zb-cloudninja-vpn-001" {
+  name                = "zb-cloudninja-vpn-001"
+  resource_group_name = "rg-keyvault-001"
+}
 
+data "azurerm_key_vault_secret" "VPNSharedSecret" {
+  name         = "VPNSharedSecret"
+  key_vault_id = data.azurerm_key_vault.zb-cloudninja-vpn-001.id
+}
+
+resource "azurerm_virtual_network_gateway_connection" "VPN-Connection" {
+  name                = "vcn-onpremises-001"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  type                       = "IPsec"
+  virtual_network_gateway_id = azurerm_virtual_network_gateway.VPN-Gateway.id
+  local_network_gateway_id   = azurerm_local_network_gateway.LocalGateway.id
+
+  shared_key = data.azurerm_key_vault_secret.VPNSharedSecret.value
 
 
