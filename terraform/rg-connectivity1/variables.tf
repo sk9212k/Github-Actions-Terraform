2ebc8b4eb6e2017611f6eb5bde3d52bc954994af
sk9212k <sk9212k@gmail.com>
2023-11-06 11:27:29 +0000

    ZB-8 I've added vnet and subnet variables to the variables.tf

--- terraform/rg-connectivity1/variables.tf
+++ terraform/rg-connectivity1/variables.tf
@@ -7,3 +7,19 @@ variable "ResourceGroup" {
     type        = string
     default     = "rg-connectivity-network-001"   
 }
+
+variable "Subnets" {
+    type = any
+    default = {
+        "GatewaySubnet" = {
+            "name"      = "GatewaySubnet"
+            "prefix"    = ["172.16.0.0/26"]
+            "routeTable" = "rt-vnet-connectivity-gateway-001"
+        }
+        "FirewallSubnet" = {
+            "name" = "AzureFirewallSubnet"
+            "prefix" = ["172.16.0.64/26"]
+            "routeTable" = "rt-vnet-connectivity-firewall-001"
+        }
+    }
+}
