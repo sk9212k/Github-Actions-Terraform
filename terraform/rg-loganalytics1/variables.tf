10aa580b9a512be96e70714f541d71f1198b29fd
sk9212k <sk9212k@gmail.com>
2023-11-07 14:13:05 +0000

    I've added logs analytics workspace configuration

--- terraform/rg-loganalytics1/variables.tf
+++ terraform/rg-loganalytics1/variables.tf
@@ -7,3 +7,12 @@ variable "ResourceGroup" {
     type = string
     default = "rg-loganalytics-001"
 }
+
+variable "LogAnalytics" {
+    type                        = any
+    default                     = {
+        "Name"                  = "log-sharedservices-001"
+        "SKU"                   = "PerGB2018"
+        "LogRentensionInDays"   = 30
+    }   
+}
